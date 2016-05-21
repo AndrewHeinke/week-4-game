8b256647d7066dd48ef4b85706720bbf89d683c7
Andrew Heinke <andrew.j.heinke@gmail.com>
2016-05-20 19:46:09 -0500

    added game reset, polished up mechanics

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
 
+ //Anytime you see swal, this references Sweet Alerts which is what I use for my alerts as an alternative to the alerts controlled by the browser
+
  // Objects within an object containing the four characters of the game
   var characters = {
     king: {
@@ -32,11 +34,24 @@ $(document).ready(function() {
     }
   };
 
+  // Global Variables
   var charSelected = false;
   var opponentsLeft = 3;
   var opponentSelected = false;
   var myChar;
   var opponentChar;
+
+  // Restarts the game
+  function restartGame() {
+    document.location.reload(true);
+  }
+
+  //Reset Button on click runs restartGame function
+  $('#resetButton').on('click', function() {
+      restartGame();
+  });
+
+
 
   // Display the images in their respective containers
   $(".king").html("<img src=" + characters.king.img + ">");
@@ -138,37 +153,48 @@ $(document).ready(function() {
 
   chooseChar();
 
-  // On click function for the attack button
+  // On click function for the attack button. Displays battle details.
   $('#attackButton').on('click', function() {
-    if (opponentSelected === true) {
-    myChar.attack = Math.round(myChar.attack * 1.1);
+    if (opponentSelected === true && myChar.health > 0 && opponentChar.health > 0) {
     opponentChar.health = opponentChar.health - myChar.attack;
     myChar.health = myChar.health - opponentChar.counterAttack;
-    console.log("Opponent Character Health: " + opponentChar.health);
-    console.log("My Character Attack: " + myChar.attack);
-    console.log("My Character Health: " + myChar.health);
+    $('.myChar-health').html('<p>Health: ' +  myChar.health + '</p>');
+    $('.opponent-health').html('<p>Health: ' +  opponentChar.health + '</p>');
+    $('.battle-details').html('<p>You attacked ' + opponentChar.name + ' for ' + myChar.attack + ' damage</p>' + '<br>' + '<p>' + opponentChar.name + ' attacked you for ' + opponentChar.counterAttack + ' damage</p>');
+    //Character attack gets 10 percent stronger each click
+    myChar.attack = Math.round(myChar.attack * 1.1);
+    }
+    else {
+      swal("Oops!", "Please select a new opponent before attacking!", "error");
+    }
+
+    //Alert dead when players character health is below 0
+    if (myChar.health <= 0 && opponentChar.health > 0) {
+      swal({title: "DEAD!",   text: "You fought well but came up short. Try harder next time!",   type: "error", confirmButtonColor: "#2ecc71",   confirmButtonText: "Restart Game", closeOnConfirm: true }, function(){restartGame();});
+    }
+
+    //Alert when both characters kill each other at the same time
+    if (myChar.health <= 0 && opponentChar.health <= 0) {
+      swal({title: "DEAD!",   text: "Your character is dead but you killed your opponent too. Congrats, but you still lost!",   type: "error", confirmButtonColor: "#2ecc71",   confirmButtonText: "Restart Game",   closeOnConfirm: true }, function(){restartGame();});
+
+    }
+
+    //When player defeats enemy, reduce opponents left count by 1, removes character from the opponent div, alerts player to select another opponent
+    if (opponentSelected === true && opponentChar.health <= 0){
+      opponentsLeft--;
+      
+      opponentSelected = false;
+      $('.chosenOpp').empty();
+      $('.opponent-health').empty();
+      $('.battle-details').empty();
+      swal("Way to go!", "You defeated " + opponentChar.name + " Now select another opponent to battle!", "success");
+      $('#instructions').html("Select a new opponent");
+    }
+
+    if (opponentsLeft === 0) {
+      //swal("Winner!", "You have defeated all of the enemies!", "success");
+      swal({title: "WINNER!",   text: "You have defeated all of the enemies!",   type: "success", confirmButtonColor: "#2ecc71",   confirmButtonText: "Restart Game",   closeOnConfirm: true }, function(){restartGame();});
     }
   });
-
-  // Restart Game Function
-  function restartGame() {
-    $('#resetButton').on('click', function() {
-      var charSelected = false;
-      var opponentsLeft = 3;
-      var opponentSelected = false;
-      var myChar;
-      var opponentChar;
-    });
-  }
-
-
-
-
-
-
-
-
-
-
 
 });
